1200f3e88d7d5704cfd772087b7ade179e283a70
RawanElGendy-code <rawankhaled305@gmail.com>
2025-12-19 16:12:13 +0200

    Delete kill-port.bat

--- kill-port.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Killing processes on port 3000...
-for /f "tokens=5" %%a in ('netstat -ano ^| findstr :3000') do (
-    echo Killing PID: %%a
-    taskkill /PID %%a /F
-)
-echo Done!
-pause
-
